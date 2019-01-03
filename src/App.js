a3633c7fa45701fab2f651c462833c3f0bff9b78
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-01-03 17:00:23 +0530

    Add basic structure of UI, using antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, Input, Form, Row, Col, Collapse } from "./components/design";
+
+const Panel = Collapse.Panel;
+
+const data = [
+  { domain: "https://www.google.com", status: "done" },
+  { domain: "https://www.facebook.com", status: "done" },
+  { domain: "https://www.amazon.com", status: "done" },
+  { domain: "https://www.youtube.com", status: "syncing" },
+  { domain: "https://www.reddit.com", status: "syncing" }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Row type="flex" justify="center">
+          <Col>
+            <h1>Url Monitor</h1>
+          </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col>
+            <Form layout="inline">
+              <Form.Item>
+                <Input placeholder="URL" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Add
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Collapse style={{ width: "50%", marginTop: "1em" }} accordion>
+            {data.map((item, index) => (
+              <Panel header={item.domain} key={index}>
+                <p>{item.domain}</p>
+              </Panel>
+            ))}
+          </Collapse>
+        </Row>
+      </React.Fragment>
     );
   }
 }
