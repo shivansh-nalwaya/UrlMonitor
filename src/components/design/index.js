9cbfefb3e1bc83da60dba1c3aa3f18d3239d38d1
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-01-03 17:03:39 +0530

    Add design components

---
+++ src/components/design/index.js
@@ -0,0 +1,8 @@
+export { default as Input } from "antd/lib/input";
+export { default as Form } from "antd/lib/form";
+export { default as Button } from "antd/lib/button";
+export { default as Row } from "antd/lib/row";
+export { default as Col } from "antd/lib/col";
+export { default as Collapse } from "antd/lib/collapse";
+export { default as Icon } from "antd/lib/icon";
+export { default as Radio } from "antd/lib/radio";
